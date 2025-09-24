df791327c4e37daeb6bbce8b70ae44ab8705618f
Nicholas Ashley <106680516+ravenitsystems@users.noreply.github.com>
2025-09-24 16:06:57 +0100

    Create react-build.sh

---
+++ EL10/react-build.sh
@@ -0,0 +1,24 @@
+##################################################################################################
+# EL10 (Alma 10) React Development Tools
+##################################################################################################
+
+# Install Node Version Manager (NVM)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+
+source ~/.bashrc
+
+nvm --version
+
+# Install node runtime
+
+nvm install 22
+
+nvm use 22
+
+nvm list
+
+# Install Yarn
+
+npm install yarn -g 
+
+
